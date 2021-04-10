f590052565c619081b75ce6cb7ab333c7683ef48
Matthew Fritsch <matthewmfritsch@gmail.com>
2021-04-09 20:28:45 -0700

    Added documentation and list changes to the PlayerInfoList and PlayerInfo classes

--- Assets/PlayerInfo.cs
+++ Assets/PlayerInfo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerInfo{
   private string playerName, characterName, characterClass;
   private int armorClass, healthPoints, initiative;
-
+//constructor assuming initiative already exists
   public PlayerInfo(string newPlayerName, string newCharacterName, string newCharacterClass, int newArmorClass, int newHealthPoints, int newInitiative){
     this.playerName = newPlayerName;
     this.characterName = newCharacterName;
@@ -14,7 +14,7 @@ public class PlayerInfo{
     this.healthPoints = newHealthPoints;
     this.initiative = newInitiative;
   }
-
+//constructor assuming initiative does not yet exist
   public PlayerInfo(string newPlayerName, string newCharacterName, string newCharacterClass, int newArmorClass, int newHealthPoints){
     this.playerName = newPlayerName;
     this.characterName = newCharacterName;
@@ -23,44 +23,64 @@ public class PlayerInfo{
     this.healthPoints = newHealthPoints;
     this.initiative = -1;
   }
-
+//accessor for the player name (string)
   public string getPlayerName(){
     return this.playerName;
   }
+//accessor for the character's name (string)
   public string getCharacterName(){
     return this.characterName;
   }
+//accessor for the character's class type (string)
   public string getCharacterClass(){
     return this.characterClass;
   }
+//accessor for the AC of the character (int32)
   public int getArmorClass(){
     return this.armorClass;
   }
+//accessor for the HP of the player (int32)
   public int getHealthPoints(){
     return this.healthPoints;
   }
+/*
+ * accessor for the initiative (int32).
+ * returns -1 if the initiative does NOT exist.
+*/ 
   public int getInitiative(){
     return this.initiative;
   }
-  
+
+
+//mutator for the name of the player
   public void setPlayerName(string newPlayerName){
     this.playerName = newPlayerName;
   }
+//mutator for the name of the character
   public void setCharacterName(string newCharacterName){
     this.characterName = newCharacterName;
   }
+//mutator for the class type of the character
   public void setCharacterClass(string newCharacterClass){
     this.characterClass = newCharacterClass;
   }
+//mutator for the AC of the character
   public void setArmorClass(int newArmorClass){
     this.armorClass = newArmorClass;
   }
+//mutator for the HP of the character
   public void setHealthPoints(int newHealthPoints){
     this.healthPoints = newHealthPoints;
   }
+//mutator for the initiative of the character
   public void setInitiative(int newInitiative){
     this.initiative = newInitiative; 
   }
-
+/*
+ * sets the initiative back to default (-1)
+*/
+  public void resetInitiative(){
+    this.initiative = -1;
+  }
 }
 
--- Assets/PlayerInfoList.cs
+++ Assets/PlayerInfoList.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 class PlayerInfoList{
 
-  private ArrayList pilist;
-
+  private List<PlayerInfo> pilist;
+//default constructor for a PlayerInfoList (creates an empty list)
   public PlayerInfoList(){
-    this.pilist = new ArrayList();
+    this.pilist = new List<PlayerInfo>();
   }
-  public ArrayList getList(){
+//returns the entire list
+  public List<PlayerInfo> getList(){
     return this.pilist;
   }
+//removes all items from the list.
   public void clearList(){
     this.pilist.Clear();
   }
+//adds a new player to the list of playerinfos.
   public void addPlayer(PlayerInfo pi){
     pilist.Add(pi);
   }
-
+//deletes a player by object from the list
   public void delPlayer(PlayerInfo pi){
     pilist.Remove(pi);
   }
-
+//deletes a player by name from the list
   public void delPlayerByName(string playername){
     for(int x = 0; x < pilist.Count; x++){
       if(pilist[x].getPlayerName() == playername){
-        pilist.removeAt(x);
+        pilist.RemoveAt(x);
         break;
       }
     }
