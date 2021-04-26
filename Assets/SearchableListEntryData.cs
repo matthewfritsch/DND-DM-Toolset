6356fa3f90cc5f568b00724f932b6b695ce4d236
Emmanuel Butor <ebutor@ucsc.edu>
2021-04-25 18:31:33 -0700

    Created prefab for base entry. Created dummy data for testing. Base functionality now works (searching for monsters and selecting them). When needed, can return a list of selected items

---
+++ Assets/SearchableListEntryData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This is a class that identifies key GameObjects in a prefab
+public class SearchableListEntryData : MonoBehaviour
+{
+	// Unity Objects
+	public Text MonsterName;
+	public Text MonsterHealth;
+	public Text MonsterArmorClass;
+}
--- Assets/SearchableMonsterList.cs
+++ Assets/SearchableMonsterList.cs
@@ -8,18 +8,136 @@ public class SearchableMonsterList : MonoBehaviour
 	// Unity Objects
 	[SerializeField]
 	private GameObject scrollView;
+	[SerializeField]
+	private GameObject scrollViewContent;	// target location to add new entries
+	[SerializeField]
+	private GameObject entryPrefab;
 
 	// Fields
-	private PlayerInfoList dummyData;
+	private PlayerInfoList data; // Currently uses dummy data
+	private Dictionary<Toggle, PlayerInfo> uiPlayerInfoMap; // maps UI elements to PlayerInfo Objects
 
 	// Unity Methods
+	void Start()
+	{
+		// initialize fields
+		uiPlayerInfoMap = new Dictionary<Toggle, PlayerInfo>();
+
+		// initialize the dummy data and create GameObjects for it
+		InitDummyData();
+		CreateGameObjectsUsingData();
+	}
 	void Update()
 	{
 	}
 
 	// Methods
+
+	// toggles the Scroll View between active and inactive
 	public void ToggleScrollView()
 	{
 		scrollView.SetActive(!scrollView.activeInHierarchy);
+	}
+
+	// converts data into GameObjects using the entryPrefab to create it.
+	//		this can be filtered by character name
+	public void CreateGameObjectsUsingData(string filter = "")
+	{
+		// clear the current dictionary
+		uiPlayerInfoMap.Clear();
+
+		// clean up the scroll view content
+		ClearScrollViewContent();
+
+		// iterate through the data
+		foreach (PlayerInfo playerInfo in data.getList())
+		{
+			// apply filter (case insensitive)
+			if (playerInfo.getCharacterName().ToLower().Contains(filter.ToLower()))
+			{
+				// create a newEntry in the scrollViewContent GameObject
+				GameObject newEntry = Instantiate(entryPrefab, scrollViewContent.transform);
+
+				// update the GameObject's text data
+				SearchableListEntryData currentEntryData = newEntry.GetComponent<SearchableListEntryData>();
+				currentEntryData.MonsterName.text = playerInfo.getCharacterName();
+				currentEntryData.MonsterHealth.text =
+					string.Format("HP: {0} / {1}", playerInfo.getCurrentHP(), playerInfo.getMaxHP());
+				currentEntryData.MonsterArmorClass.text = string.Format("Armor Class: {0}", playerInfo.getArmorClass());
+
+				// establish GameObject to PlayerInfo mapping
+				uiPlayerInfoMap.Add(newEntry.GetComponent<Toggle>(), playerInfo);
+			}
+		}
+	}
+
+	// deletes everything from the scroll view content
+	public void ClearScrollViewContent(bool ignoreSelected = false)
+	{
+		for (int idx = 0; idx < scrollViewContent.transform.childCount; idx++)
+		{
+			GameObject child = scrollViewContent.transform.GetChild(idx).gameObject;
+			Destroy(child);
+		}
+	}
+
+	// reads from the Content GameObject and returns a PlayerInfoList of all selected PlayerInfos
+	public PlayerInfoList GetSelectedMonsters()
+	{
+		// create a new empty list
+		PlayerInfoList selectedMonsters = new PlayerInfoList();
+
+		// loop through the mapping dictionary
+		foreach (KeyValuePair<Toggle, PlayerInfo> pair in uiPlayerInfoMap)
+		{
+			// if a Toggle is switched to On, add that to the list to return
+			if (pair.Key.isOn)
+			{
+				selectedMonsters.addPlayer(pair.Value);
+			}
+		}
+
+		// return the selected monsters
+		return selectedMonsters;
+	}
+
+	// runs GetSelectedMonsters and prints the value that it returns
+	public void PrintSelectedMonsters()
+	{
+		// get the selected monsters
+		PlayerInfoList selectedMonsters = GetSelectedMonsters();
+
+		// create an output string
+		string output = "Selected Monsters: ...\n";
+
+		foreach (PlayerInfo playerInfo in selectedMonsters.getList())
+		{
+			output += string.Format("{0}: HP({1}/{2}), ArmorClass({3})\n",
+				playerInfo.getCharacterName(),
+				playerInfo.getCurrentHP(),
+				playerInfo.getMaxHP(),
+				playerInfo.getArmorClass());
+		}
+
+		// print the output
+		Debug.Log(output);
+	}
+
+	// Helper Methods
+
+	// initializes the dummy data
+	private void InitDummyData()
+	{
+		// Construct new dummyData
+		data = new PlayerInfoList();
+
+		// Add new data
+		data.addPlayer(new PlayerInfo("MONSTER", "Tanky Being", "MONSTER", 20, 100));
+		data.addPlayer(new PlayerInfo("MONSTER", "Tanky Being 2", "MONSTER", 40, 60));
+		data.addPlayer(new PlayerInfo("MONSTER", "Weak Being", "MONSTER", 5, 20));
+		data.addPlayer(new PlayerInfo("MONSTER", "Weak Being 2", "MONSTER", 10, 10));
+		data.addPlayer(new PlayerInfo("MONSTER", "Boss Monster", "MONSTER", 45, 200));
+		data.addPlayer(new PlayerInfo("MONSTER", "Boss Monster 2", "MONSTER", 65, 400));
+		data.addPlayer(new PlayerInfo("MONSTER", "Boss Monster 3", "MONSTER", 80, 800));
 	}
 }
